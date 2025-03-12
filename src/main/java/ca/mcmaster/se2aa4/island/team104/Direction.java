8b6e784668913316b03b8860a87365cfc6a2d0a6
Mellostar25 <67835601+Harsh-Is-Coding@users.noreply.github.com>
2025-03-12 15:14:37 -0400

    added direction translator

--- src/main/java/ca/mcmaster/se2aa4/island/team104/Direction.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team104/Direction.java
@@ -27,11 +27,21 @@ public enum Direction {
     @Override
     public String toString() {
         return switch (this) {
-            case North -> "N";
-            case South -> "S";
-            case East -> "E";
-            case West -> "W";
+            case North -> "NORTH";
+            case South -> "SOUTH";
+            case East -> "EAST";
+            case West -> "WEST";
             default -> "NONE";
+        };
+    }
+
+    public static Direction directionFromString(String direction) {
+        return switch (direction) {
+            case "NORTH" -> Direction.North;
+            case "SOUTH" -> Direction.South;
+            case "EAST" -> Direction.East;
+            case "WEST" -> Direction.West;
+            default -> Direction.NONE;
         };
     }
 
--- src/main/java/ca/mcmaster/se2aa4/island/team104/Drone.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team104/Drone.java
@@ -4,15 +4,17 @@ import ca.mcmaster.se2aa4.island.team104.actions.Action;
 
 public class Drone {
     protected Battery battery;
-    protected String direction;
+    protected Direction direction;
+    protected CoordinateSystem coordinates;
 
-    public Drone(int batteryLevel, String direction) {
+    public Drone(int batteryLevel, String direction, int x, int y) {
         this.battery = new Battery(batteryLevel);
-        this.direction = direction;
+        this.direction = Direction.directionFromString(direction);
+        this.coordinates = new CoordinateSystem(x, y, Direction.directionFromString(direction));
     }
 
 
-    public String getDirection() {
+    public Direction getDirection() {
         return direction;
     }
 
@@ -23,7 +25,7 @@ public class Drone {
     }
 
     private void setDirection(String direction) {
-        this.direction = direction;
+        this.direction = Direction.directionFromString(direction);
     }
 
 }
